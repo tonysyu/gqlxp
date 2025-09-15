9708347600b0c4b8116685d7df603cb4ee07a765
Tony S Yu <tsyu80@gmail.com>
2025-09-14 23:07:08 -0500

    Refactor code into cmd and tui modules

--- tui/model.go
+++ tui/model.go
@@ -1,30 +1,29 @@
-package main
+package tui
 
 import (
 	"fmt"
-	"os"
 
 	tea "github.com/charmbracelet/bubbletea"
 )
 
-type model struct {
+type Model struct {
 	choices  []string
 	cursor   int
 	selected map[int]struct{}
 }
 
-func initialModel() model {
-	return model{
+func NewModel() Model {
+	return Model{
 		choices:  []string{"Buy carrots", "Buy celery", "Buy kohlrabi"},
 		selected: make(map[int]struct{}),
 	}
 }
 
-func (m model) Init() tea.Cmd {
+func (m Model) Init() tea.Cmd {
 	return nil
 }
 
-func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+func (m Model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	switch msg := msg.(type) {
 	case tea.KeyMsg:
 		switch msg.String() {
@@ -50,7 +49,7 @@ func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	return m, nil
 }
 
-func (m model) View() string {
+func (m Model) View() string {
 	s := "What should we buy at the market?\n\n"
 
 	for i, choice := range m.choices {
@@ -69,12 +68,4 @@ func (m model) View() string {
 
 	s += "\nPress q to quit.\n"
 	return s
-}
-
-func main() {
-	p := tea.NewProgram(initialModel())
-	if _, err := p.Run(); err != nil {
-		fmt.Printf("Alas, there's been an error: %v", err)
-		os.Exit(1)
-	}
 }
